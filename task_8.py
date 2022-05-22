5b5cb4c44521dfc1d0de720b81e8ed58833e109e
PolinaMikh <polli030600@mail.ru>
2022-05-22 21:51:25 +0500

    release v1

---
+++ task_8.py
@@ -0,0 +1,17 @@
+import vk_api
+
+with open('token.txt') as f:
+    my_token = f.readline()
+
+session = vk_api.VkApi(token=my_token)
+vk = session.get_api()
+
+with open('id.txt') as f:
+    user_id = f.read()
+
+for id in vk.friends.get(user_id=user_id)['items']:
+    user = vk.users.get(user_ids=id)[0]
+    print(user['first_name'], user['last_name'])
+
+
+
