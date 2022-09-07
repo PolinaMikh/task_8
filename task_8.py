5d14979ad6ba26ca07def998a9d131032a21cfd5
PolinaMikh <84188819+PolinaMikh@users.noreply.github.com>
2022-09-07 08:54:02 +0000

    Update task_8.py

--- task_8.py
+++ task_8.py
@@ -1,14 +1,16 @@
-import vk_api
-
-with open('token.txt') as f:
-    my_token = f.readline()
-
-session = vk_api.VkApi(token=my_token)
-vk = session.get_api()
+import requests
 
 with open('id.txt') as f:
-    user_id = f.read()
+    id = f.read()
 
-for id in vk.friends.get(user_id=user_id)['items']:
-    user = vk.users.get(user_ids=id)[0]
-    print(user['first_name'], user['last_name'])
+with open('token.txt') as f:
+    my_token = f.read()
+
+result = requests.get('https://api.vk.com/method/friends.get?user_ids=' +
+                        id + '&fields=name&access_token=' + my_token + '&v=5.131').json()
+i = 0
+for i in range(len(result['response']['items'])):
+    first_name = result['response']['items'][i]['first_name']
+    last_name = result['response']['items'][i]['last_name']
+    print(first_name + " " + last_name)
+    i += i
